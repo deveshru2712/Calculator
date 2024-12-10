52848f76193f75b7d433950e86630409fa4749f3
yashdev2712 <deveshchandra2712@gmail.com>
2024-12-10 08:37:58 +0530

    fix minor issue

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -1,8 +1,12 @@
 import React from "react";
 
-const Button = ({ value, color }) => {
+const Button = ({ value, color, onclick }) => {
   return (
     <button
+      onClick={() => {
+        onclick(value);
+      }}
+      value={value}
       className={`active:scale-125 duration-200 flex justify-center items-center text-2xl text-black font-semibold border  border-white ${color}`}
     >
       {value}
--- src/components/Display.jsx
+++ src/components/Display.jsx
@@ -1,38 +1,79 @@
-import React from "react";
+import React, { useReducer, useState } from "react";
 import Button from "./Button";
 
+const reducer = (state, action) => {};
+
 const Display = () => {
+  const [currentOperannd, setCurrentOperand] = useState(0);
+  const [previousOperand, setPreviousOperand] = useState(0);
+
+  const [state, dispatch] = useReducer(reducer, currentOperannd);
+
+  const handleButtonClicked = (value) => {
+    setCurrentOperand(value);
+  };
+
+  const allClearHandler = () => {
+    setCurrentOperand(0);
+    setPreviousOperand(0);
+  };
+
+  const calculate = () => {
+    setPreviousOperand(currentOperannd);
+    setCurrentOperand(0);
+  };
+
   return (
     <div className="flex flex-col justify-center items-center h-full w-full">
-      <div className="bg-display h-1/6 w-1/3 text-white font-semibold text-2xl text-right flex flex-col justify-between items-stretch border border-white p-4">
-        <div className="post"></div>
-        <div className="current">00</div>
+      <div className="bg-display h-1/6 w-1/3 text-white font-semibold text-2xl text-right flex flex-col justify-between items-stretch border border-white p-4 overflow-hidden">
+        <div className="post">{previousOperand}</div>
+        <div className="current">{currentOperannd}</div>
       </div>
       <div className="w-1/3 h-1/2 grid grid-cols-4 grid-rows-5 border border-white">
-        <Button value={"AC"} color={"bg-gray"} />
-        <Button value={"+/-"} color={"bg-gray"} />
-        <Button value={"%"} color={"bg-gray"} />
-        <Button value={"/"} color={"bg-buttonOrange"} />
+        <Button value={"AC"} color={"bg-slate"} onclick={allClearHandler} />
+        <Button
+          value={"+/-"}
+          color={"bg-slate"}
+          onclick={handleButtonClicked}
+        />
+        <Button value={"%"} color={"bg-slate"} onclick={handleButtonClicked} />
+        <Button
+          value={"/"}
+          color={"bg-buttonOrange"}
+          onclick={handleButtonClicked}
+        />
 
-        <Button value={"7"} color={"bg-gray"} />
-        <Button value={"8"} color={"bg-gray"} />
-        <Button value={"9"} color={"bg-gray"} />
-        <Button value={"*"} color={"bg-buttonOrange"} />
+        <Button value={"7"} color={"bg-gray"} onclick={handleButtonClicked} />
+        <Button value={"8"} color={"bg-gray"} onclick={handleButtonClicked} />
+        <Button value={"9"} color={"bg-gray"} onclick={handleButtonClicked} />
+        <Button
+          value={"*"}
+          color={"bg-buttonOrange"}
+          onclick={handleButtonClicked}
+        />
 
-        <Button value={"4"} color={"bg-gray"} />
-        <Button value={"5"} color={"bg-gray"} />
-        <Button value={"6"} color={"bg-gray"} />
-        <Button value={"-"} color={"bg-buttonOrange"} />
+        <Button value={"4"} color={"bg-gray"} onclick={handleButtonClicked} />
+        <Button value={"5"} color={"bg-gray"} onclick={handleButtonClicked} />
+        <Button value={"6"} color={"bg-gray"} onclick={handleButtonClicked} />
+        <Button
+          value={"-"}
+          color={"bg-buttonOrange"}
+          onclick={handleButtonClicked}
+        />
 
-        <Button value={"3"} color={"bg-gray"} />
-        <Button value={"2"} color={"bg-gray"} />
-        <Button value={"1"} color={"bg-gray"} />
-        <Button value={"÷"} color={"bg-buttonOrange"} />
+        <Button value={"3"} color={"bg-gray"} onclick={handleButtonClicked} />
+        <Button value={"2"} color={"bg-gray"} onclick={handleButtonClicked} />
+        <Button value={"1"} color={"bg-gray"} onclick={handleButtonClicked} />
+        <Button
+          value={"÷"}
+          color={"bg-buttonOrange"}
+          onclick={handleButtonClicked}
+        />
 
-        <Button value={"0"} color={"bg-gray"} />
-        <Button value={"."} color={"bg-gray"} />
-        <Button value={"C"} color={"bg-gray"} />
-        <Button value={"="} color={"bg-equal"} />
+        <Button value={"0"} color={"bg-gray"} onclick={handleButtonClicked} />
+        <Button value={"."} color={"bg-gray"} onclick={handleButtonClicked} />
+        <Button value={"C"} color={"bg-gray"} onclick={handleButtonClicked} />
+        <Button value={"="} color={"bg-equal"} onclick={calculate} />
       </div>
     </div>
   );
